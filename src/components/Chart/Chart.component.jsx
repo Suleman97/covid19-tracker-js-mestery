05a7224672cbcefecac6e0992fc04ae422daf48a
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-07-07 22:39:54 +0500

    Line chart is setup for Covid19 data representation

--- src/components/Chart/Chart.component.jsx
+++ src/components/Chart/Chart.component.jsx
@@ -1,10 +1,66 @@
+import { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 
+import './Chart.styles.css';
 
-export const Chart = () => {
+export const Chart = ({ data, country }) => {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  }, []);
+
+  // console.log(dailyData[2]);
+  // const LineChart = <Line data={{
+  //   labels: dailyData.map(({ date }) => date),
+  //   datasets: [
+  //     {
+  //       label: 'Infected',
+  //       data: dailyData.map(({ confirmed }) => confirmed),
+  //       fill: false,
+  //       backgroundColor: 'rgb(255, 99, 132)',
+  //       borderColor: 'rgba(255, 99, 132, 0.2)',
+  //     },
+  //     {
+  //       data: dailyData.map(({ deaths }) => deaths),
+  //       label: 'deaths',
+  //       borderColor: 'red',
+  //       backgroundColor: 'rgba(255, 0, 0, 0.5',
+  //       fill: false,
+  //     }
+  //   ],
+  // }} />
+  const LineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#333fff',
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5',
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
   return (
-    <div>
-      Chart
+    <div className="container">
+      {/* <LineChart /> */}
+      {LineChart}
     </div>
-  )
-}
-
+  );
+};
